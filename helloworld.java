ef900727288cd227cb560526c382f399527d745e
Jayaprakash (JP) Balakrishnan <jayaprakash.balakrishnan@gmail.com>
2022-02-08 20:19:33 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World polling is working again from github1!");
+    System.out.println("Hello World polling is working again from github2!");
     }
 }
