f0c27887b81e7140c73141afc3da71bdc8d5dde2
Jayaprakash (JP) Balakrishnan <jayaprakash.balakrishnan@gmail.com>
2022-02-08 15:42:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World and the build triggered by webhook");
+    System.out.println("Hello World polling is working!");
     }
 }
