2f68252bc204af9a289e19b1c51b9e1bc616eecf
Jayaprakash (JP) Balakrishnan <jayaprakash.balakrishnan@gmail.com>
2022-02-09 07:30:46 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World : Polling Test1!");
+    System.out.println("Hello World : Polling Test2!");
     }
 }
