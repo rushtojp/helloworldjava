a09d2c6a755f1135d09884f15e1699148cdca958
Jayaprakash (JP) Balakrishnan <jayaprakash.balakrishnan@gmail.com>
2022-05-05 08:32:00 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from GitHub usine webhooks");
+    System.out.println("Hello World from DevOps");
     }
 }
