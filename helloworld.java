17707732f6b3ef0eed280de64b24f798acfc7b39
Jayaprakash (JP) Balakrishnan <jayaprakash.balakrishnan@gmail.com>
2022-06-08 08:31:26 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 8th june - 08:23 AM!");
+    System.out.println("Hello World 8th june - 08:23 AM - webhooks demo1!");
     }
 }
