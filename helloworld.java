5fb5bd00d52e096a8ea052c3d41abe0ba2a3437f
Jayaprakash (JP) Balakrishnan <jayaprakash.balakrishnan@gmail.com>
2022-06-08 08:06:56 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 7th june 2022 - new update!");
+    System.out.println("Hello World 8th june!");
     }
 }
