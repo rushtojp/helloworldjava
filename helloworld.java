e0421ed122e91d4b12dc1f04cd8d1dd231c2afc5
Jayaprakash (JP) Balakrishnan <jayaprakash.balakrishnan@gmail.com>
2022-06-19 22:14:02 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 19th june 10:11 PM IST updated");
+    System.out.println("Hello World 19th june 10:13 PM IST updated");
     }
 }
