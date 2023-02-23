202453fbc03a614de90dd531d2650530874e9f20
Maddy-ria <76203499+Maddy-ria@users.noreply.github.com>
2023-02-24 02:15:24 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Feb 19th 2023 - 09:05PM - triggered by webhook");
+    System.out.println("Hello World - Feb 24th 2023 - 09:05PM - triggered by webhook");
     }
 }
