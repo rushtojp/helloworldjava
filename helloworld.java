4507f5d73c07594dea1459bb1db85248462ef6f4
Maddy-ria <76203499+Maddy-ria@users.noreply.github.com>
2023-05-13 12:16:27 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - May 14th 2023 - 10:34 AM - triggered by todays 2d webhok");
+    System.out.println("Hello World - May 11th 2023 - 10:34 AM - triggered by todays 2d webhok");
     }
 }
